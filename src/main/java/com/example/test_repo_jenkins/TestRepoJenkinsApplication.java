bcd866a119e6f41cdb52a8599e946840e89a56e8
hrithik-b01 <hrithik.b@codecraft.co.in>
2024-12-26 15:30:30 +0530

    initial commit

---
+++ src/main/java/com/example/test_repo_jenkins/TestRepoJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_repo_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRepoJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_repo_jenkins/TestRepoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_repo_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
